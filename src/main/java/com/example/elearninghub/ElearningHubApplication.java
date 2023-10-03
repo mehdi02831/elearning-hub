97b493470aa2f4a31e26157e273b2936cf9b1540
mehdi <mehdi@gmail.com>
2023-10-03 02:09:07 +0100

    Initial commit

---
+++ src/main/java/com/example/elearninghub/ElearningHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.elearninghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearningHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elearninghub/ElearningHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elearninghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
